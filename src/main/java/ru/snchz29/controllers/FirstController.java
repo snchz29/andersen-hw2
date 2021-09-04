92fcc098db4c7fdaf37fdde1107b5d614806fc96
Alexander Terekhov <sasha4888.terehov@gmail.com>
2021-09-04 14:38:36 +0300

    initial commit

---
+++ src/main/java/ru/snchz29/controllers/FirstController.java
@@ -0,0 +1,56 @@
+package ru.snchz29.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+@RequestMapping("/first")
+public class FirstController {
+    @GetMapping("/hello")
+    public String helloPage(@RequestParam(value = "name", required = false) String name,
+                            @RequestParam(value = "surname", required = false) String surname,
+                            Model model) {
+//        System.out.println(name + " " + surname);
+        model.addAttribute("message", "Hello, " + name + " " + surname);
+        return "first/hello";
+    }
+
+    @GetMapping("/bye")
+    public String goodbyePage() {
+        return "first/goodbye";
+    }
+
+    @GetMapping("/calc")
+    public String calc(@RequestParam(value = "a") int a,
+                       @RequestParam(value = "b") int b,
+                       @RequestParam(value = "action") String action,
+                       Model model) {
+        double result = 0;
+        String act = "...";
+        switch (action) {
+            case "multiplication":
+                act = "*";
+                result = a * b;
+                break;
+            case "division":
+                act = "/";
+                result = a / (double)b;
+                break;
+            case "addition":
+                act = "+";
+                result = a + b;
+                break;
+            case "subtraction":
+                act = "-";
+                result = a - b;
+                break;
+        }
+        model.addAttribute("result", "" + a + act + b + "=" + result);
+        return "first/calc";
+    }
+}
---
+++ src/main/java/ru/snchz29/controllers/SecondController.java
@@ -0,0 +1,12 @@
+package ru.snchz29.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class SecondController {
+    @GetMapping("/exit")
+    public String exit() {
+        return "second/exit";
+    }
+}
