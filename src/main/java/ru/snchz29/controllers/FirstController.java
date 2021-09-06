faa83cb8d79cb7dd3e73d773f4a85c95ccbfdb0d
Alexander Terekhov <42890729+snchz29@users.noreply.github.com>
2021-09-06 14:28:47 +0300

    Update FirstController.java

--- src/main/java/ru/snchz29/controllers/FirstController.java
+++ src/main/java/ru/snchz29/controllers/FirstController.java
@@ -12,12 +12,8 @@ import javax.servlet.http.HttpServletRequest;
 @RequestMapping("")
 public class FirstController {
     @GetMapping("/hello")
-    public String helloPage(@RequestParam(value = "name", required = false) String name,
-                            @RequestParam(value = "surname", required = false) String surname,
-                            Model model) {
-//        System.out.println(name + " " + surname);
-        model.addAttribute("message", "Hello, " + name + " " + surname);
-        return "first/hello";
+    public String helloPage() {
+        return "hello_world";
     }
 
     @GetMapping("")
