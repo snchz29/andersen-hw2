3f4a910575523eaa189336a2424feee0fc65cc53
Alexander Terekhov <sasha4888.terehov@gmail.com>
2021-09-10 22:48:37 +0300

    add toString method

--- src/main/java/ru/snchz29/models/User.java
+++ src/main/java/ru/snchz29/models/User.java
@@ -1,10 +1,12 @@
 package ru.snchz29.models;
 
 import lombok.Data;
+import lombok.ToString;
 
 import java.time.LocalDateTime;
 
 @Data
+@ToString
 public class User {
     private long id;
     private String name;
