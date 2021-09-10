12f774883065817d966b9e3d520206396ceb1500
Alexander Terekhov <sasha4888.terehov@gmail.com>
2021-09-10 23:47:37 +0300

    refactoring

--- src/main/java/ru/snchz29/servlets/actions/AbstractAction.java
+++ src/main/java/ru/snchz29/servlets/actions/AbstractAction.java
@@ -6,8 +6,9 @@ import ru.snchz29.dao.UsersDaoImpl;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public abstract class AbstractAction implements Action{
+public abstract class AbstractAction implements Action {
     protected UsersDao dao = new UsersDaoImpl();
+
     @Override
     public abstract String exec(HttpServletRequest request, HttpServletResponse response);
 }
