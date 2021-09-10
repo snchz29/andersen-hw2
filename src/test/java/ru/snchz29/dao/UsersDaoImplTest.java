f7f7b27f49ab47348fd79f140009fc2fff47df6f
Alexander Terekhov <sasha4888.terehov@gmail.com>
2021-09-10 22:46:30 +0300

    add some basic test classes

---
+++ src/test/java/ru/snchz29/dao/UsersDaoImplTest.java
@@ -0,0 +1,36 @@
+package ru.snchz29.dao;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UsersDaoImplTest {
+
+    @Test
+    void open() {
+    }
+
+    @Test
+    void close() {
+    }
+
+    @Test
+    void insertUser() {
+    }
+
+    @Test
+    void getUserById() {
+    }
+
+    @Test
+    void getAllUsers() {
+    }
+
+    @Test
+    void updateUser() {
+    }
+
+    @Test
+    void deleteUser() {
+    }
+}
