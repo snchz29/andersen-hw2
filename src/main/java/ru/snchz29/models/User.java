90e79fdffcc1f141e57c7a5ed3b07f1297a099da
Alexander Terekhov <sasha4888.terehov@gmail.com>
2021-09-11 13:44:20 +0300

    add simple input control

--- src/main/java/ru/snchz29/models/User.java
+++ src/main/java/ru/snchz29/models/User.java
@@ -1,9 +1,13 @@
 package ru.snchz29.models;
 
+import lombok.AccessLevel;
 import lombok.Data;
+import lombok.Setter;
 import lombok.ToString;
 
 import java.time.LocalDateTime;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @Data
 @ToString
@@ -11,9 +15,27 @@ public class User {
     private long id;
     private String name;
     private String surname;
+    @Setter(AccessLevel.NONE)
     private int age;
+    @Setter(AccessLevel.NONE)
     private String email;
     private LocalDateTime timeCreated;
     private LocalDateTime lastUpdated;
     private boolean isDeleted;
+
+    public void setAge(int age) {
+        if (age < 0) {
+            throw new IllegalArgumentException("Age must be greater than 0");
+        }
+        this.age = age;
+    }
+
+    public void setEmail(String email) {
+        Pattern pattern = Pattern.compile("");
+        Matcher matcher = pattern.matcher(email);
+        if (!matcher.matches()) {
+            throw new IllegalArgumentException("Incorrect email");
+        }
+        this.email = email;
+    }
 }
